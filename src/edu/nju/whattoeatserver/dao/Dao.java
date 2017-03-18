46364b9c59b2a6fc560f38fd96894d8f36da137c
RiverXu <sdxujianghe@126.com>
2017-03-18 19:05:06 +0800

    Example class commit

---
+++ src/edu/nju/whattoeatserver/dao/Dao.java
@@ -0,0 +1,7 @@
+package edu.nju.whattoeatserver.dao;
+
+/**
+ * Created by 徐江河 on 2017/3/18.
+ */
+public class Dao {
+}
---
+++ src/edu/nju/whattoeatserver/servlet/Servlet.java
@@ -0,0 +1,7 @@
+package edu.nju.whattoeatserver.servlet;
+
+/**
+ * Created by 徐江河 on 2017/3/18.
+ */
+public class Servlet {
+}
